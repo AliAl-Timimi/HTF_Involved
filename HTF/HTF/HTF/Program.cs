282cb443a9402d46a410b24bd72f82cb0786d16f
Ali Al-Timimi <ali.al-timimi@student.kdg.be>
2021-12-02 10:25:03 +0100

    init

---
+++ HTF/HTF/HTF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTF.HTF
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
